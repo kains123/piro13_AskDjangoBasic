5e3d6f7a39aef70021b8ab8df751b6389b960cc3
LeeYeJin <kains0417@naver.com>
2020-07-24 21:51:15 +0900

    25 static 파일을 다루는 방법

--- mysite/settings.py
+++ mysite/settings.py
@@ -147,6 +147,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'mysite', 'static'),
+]
+
 INTERNAL_IPS = [
     '127.0.0.1']
 
